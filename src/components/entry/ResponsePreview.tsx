da927d706142e11ed4326cc73c5ac601407a91ec
darkruby <art@darkruby.co.uk>
2023-10-17 20:20:21 +0100

    added vscode commands

--- src/components/entry/ResponsePreview.tsx
+++ src/components/entry/ResponsePreview.tsx
@@ -1,15 +1,7 @@
 import { format } from "date-fns";
 import { pipe } from "fp-ts/lib/function";
 import ReactMarkdown from "react-markdown";
-import {
-  Badge,
-  Card,
-  CardBody,
-  CardTitle,
-  Col,
-  Row,
-  TabPane,
-} from "reactstrap";
+import { Card, CardBody, CardTitle, Col, Row, TabPane } from "reactstrap";
 import remarkGfm from "remark-gfm";
 import {
   ALL_MODELS,
@@ -28,34 +20,44 @@ export type ResponsePreviewProps = {
 
 const RawResponsePreview: React.FC<ResponsePreviewProps> = ({ response }) => {
   return (
-    <Row>
-      <Col>
-        <Badge>{format(response.created, "yyyy-MM-dd HH:mm:ss")}</Badge>
-        <Select<ChatCompletionsModel>
-          options={ALL_MODELS}
-          selected={response.model}
-          disabled
-        />
-        {response.choices.map(({ index, message }) => (
-          <Card key={index}>
-            <CardTitle>{message.role}</CardTitle>
-            <CardBody>
-              <Tabs tabs={["Preview", "Raw"]}>
-                <TabPane tabId={0}>
-                  <ReactMarkdown
-                    children={message.content}
-                    remarkPlugins={[remarkGfm]}
-                  />
-                </TabPane>
-                <TabPane tabId={1}>
-                  <TextArea rows={20} value={message.content} readOnly />
-                </TabPane>
-              </Tabs>
-            </CardBody>
-          </Card>
-        ))}
-      </Col>
-    </Row>
+    <>
+      <Row>
+        <Col xs={4}>
+          <Select<ChatCompletionsModel>
+            options={ALL_MODELS}
+            selected={response.model}
+            disabled
+          />
+        </Col>
+        <Col xs={4}>
+          <span>{format(response.created, "yyyy-MM-dd HH:mm:ss")}</span>
+        </Col>
+        <Col xs={4}>
+          <span>prompt tokens: {response.usage.prompt_tokens}</span> &nbsp;
+          <span>completion tokens: {response.usage.completion_tokens}</span>
+        </Col>
+      </Row>
+      {response.choices.map(({ index, message }) => (
+        <Card key={index}>
+          <CardTitle>
+            <h6>role: {message.role}</h6>
+          </CardTitle>
+          <CardBody>
+            <Tabs tabs={["Preview", "Raw"]}>
+              <TabPane tabId={0}>
+                <ReactMarkdown
+                  children={message.content}
+                  remarkPlugins={[remarkGfm]}
+                />
+              </TabPane>
+              <TabPane tabId={1}>
+                <TextArea rows={20} value={message.content} readOnly />
+              </TabPane>
+            </Tabs>
+          </CardBody>
+        </Card>
+      ))}
+    </>
   );
 };
 
